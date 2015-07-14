a5624461e274557c07b899eb93a89581cae0c14c
Jack Polgar <jack@polgar.id.au>
2015-07-14 14:06:26 +1000

    Update namespace.

--- src/Hook.php
+++ src/Hook.php
@@ -1,13 +1,13 @@
 <?php
-/*!
- * Radium
- * Copyright 2011-2014 Jack Polgar
+/*
+ * Avalon
+ * Copyright 2011-2015 Jack Polgar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,14 +16,14 @@
  * limitations under the License.
  */
 
-namespace Radium;
+namespace Avalon;
 
 /**
  * The FishHook plugin library
  *
  * @package FishHook
  * @author Jack P.
- * @copyright (C) 2009-2012 Jack P.
+ * @copyright (C) 2009-2015 Jack P.
  * @version 4.0
  */
 class Hook
